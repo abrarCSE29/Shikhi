d13fbf9a9eebe12dcc72ff195ffa4c8c3913fa89
abrarCSE29 <96782774+abrarCSE29@users.noreply.github.com>
2024-06-14 00:45:17 +0600

    created Homepage and added courses in the homepage

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -3,14 +3,23 @@ import React from 'react'
 import './Homepage.css'
 import Course from '../Course/Course'
 import Cart from '../Cart/Cart'
+import courses from './courses'
 export default function Homepage() {
+
+  //const [allcourse,setAllCourses] = useState([]);
+ 
+
   return (
     <Box
       className='container'
     >
       <Grid container spacing={2}>
         <Grid item xs={8}>
-          <Course></Course>
+          {
+            courses.map((course,index)=>
+              <Course key={index} course={course}></Course>
+            )
+          }
         </Grid>
         <Grid item xs={4}>
           <Cart></Cart>
