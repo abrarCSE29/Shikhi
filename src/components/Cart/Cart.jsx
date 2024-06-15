bde511008c1ecaeeb972b0373dbfd693b45c544e
abrarCSE29 <jucse29.398@gmail.com>
2024-06-15 17:45:07 +0600

    Added delete icon in cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,12 +1,13 @@
-import { Box, List, ListItem, ListItemText, Paper, Typography } from '@mui/material'
+import { Box, Button, List, ListItem, ListItemText, Paper, Typography } from '@mui/material'
 import React from 'react'
 import './Cart.css'
+import DeleteIcon from '@mui/icons-material/Delete';
 export default function Cart(props) {
 
   const cartCourses = props.cartCourses;
   // console.log(props);
 
-  const grandTotal = cartCourses.reduce((sum,tmp)=> sum+tmp.price,0);
+  const grandTotal = cartCourses.reduce((sum, tmp) => sum + tmp.price, 0);
   return (
     <Box
       className='cart-container'
@@ -39,16 +40,27 @@ export default function Cart(props) {
                       display={'flex'}
                       justifyContent={'space-between'}
                     >
-                      <Box>
+                      <Box
+                        sx={{
+                          display : 'flex',
+                          justifyContent : 'flex-start',
+                          alignItems : 'center'
+                        }}
+                      >
+                        <DeleteIcon/>
+                        <Box>
                         <ListItem >
                           <ListItemText className='bold'>{course.name}</ListItemText>
                         </ListItem>
                       </Box>
+                      </Box>
+                      
                       <Box>
                         <ListItem >
                           <ListItemText className='bold'>Tk {course.price}</ListItemText>
                         </ListItem>
                       </Box>
+
                     </Box>
 
                     {
