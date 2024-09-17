63c29b1dce1b074ee235afcfa87834a80777ffe9
abrarCSE29 <jucse29.398@gmail.com>
2024-09-18 00:05:43 +0600

    updated ViewCourses.jsx

--- src/components/Course/VIewCourses.jsx
+++ src/components/Course/VIewCourses.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
-import courses from '../Homepage/courses'
+import React, { useEffect, useState } from 'react'
+// import courses from '../Homepage/courses'
 import { Box, Grid } from '@mui/material'
 import Course from './Course'
+import axios from 'axios';
 export default function VIewCourses() {
+    const [allCourses, setAllCourses] = useState([]);
+    useEffect(() => {
+        // Using axios
+        axios.get('http://localhost:5000/courses')
+          .then(response => {
+            setAllCourses(response.data);
+          })
+          .catch(error => {
+            console.error('Error fetching courses:', error);
+          });
+    
+        // Alternatively, you can use fetch
+        /*
+        fetch('http://localhost:5000/courses')
+          .then(response => response.json())
+          .then(data => setAllCourses(data))
+          .catch(error => console.error('Error fetching courses:', error));
+        */
+      }, []);
+
     return (
         <>
             <Box
@@ -11,7 +32,7 @@ export default function VIewCourses() {
                 <Grid container spacing={2}>
                     <Grid item xs={12}>
                         {
-                            courses.map((course,index)=>
+                            allCourses.map((course,index)=>
                                 <Course key={index} course={course}></Course>
                             )
                         }
